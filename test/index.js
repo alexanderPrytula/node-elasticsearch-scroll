15f9f4ae3b3896f43d2d73c3c748cdb2c94a0125
Mike Atkins <mike.atkins@lanetix.com>
2015-02-02 15:13:00 -0500

    Increase the timeout further
    apparently travis is  *very* slow.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ var elasticsearch = require('elasticsearch'),
 
 describe('When 20 things are indexed', function () {
   beforeEach(function () {
-    this.timeout(3000);
+    this.timeout(4000);
     return BPromise.map(range(0, 20), function (number) {
       return client.index({
         index: 'elasticsearch-scroll-test',
