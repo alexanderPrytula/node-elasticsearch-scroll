e835a93b1d51f29652eec2710a550371a075e63d
Mike Atkins <mike.atkins@lanetix.com>
2015-02-02 11:20:08 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# node-elasticsearch-scroll
+node-elasticsearch-scroll
+=======
+WIP
---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var curry = require('lodash.curry');
+
+module.exports = function (client) {
+  var scrollToEnd = curry(function (resultTransform, results, res) {
+    results = results.concat(resultTransform(res.hits.hits));
+
+    if (results.length < res.hits.total) {
+      return client.scroll({
+        /* eslint-disable no-underscore-dangle */
+        scrollId: res._scroll_id,
+        scroll: '30s'
+        /* eslint-enable no-underscore-dangle */
+      })
+        .then(scrollToEnd(resultTransform, results));
+    }
+    else {
+      return results;
+    }
+  });
+};
