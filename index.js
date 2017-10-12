d3dc109bea01f4c705391ce5f672d7f824d23e9e
Alexander Prytula <isitinme88@gmail.com>
2017-10-12 17:28:21 +0300

    allow params and clearScroll

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 'use strict';
 
-var curry = require('lodash.curry');
+const curry = require('lodash.curry');
 
-module.exports = function (client) {
-  var scrollToEnd = curry(function (resultTransform, results, res) {
+module.exports = (client, {scrollTimeout = '30s'}) => {
+  const scrollToEnd = curry((resultTransform, results, res) => {
     results = results.concat(resultTransform(res.hits.hits));
 
     if (results.length < res.hits.total) {
       return client.scroll({
         /* eslint-disable no-underscore-dangle */
         scrollId: res._scroll_id,
-        scroll: '30s'
+        scroll: scrollTimeout
         /* eslint-enable no-underscore-dangle */
       })
         .then(scrollToEnd(resultTransform, results));
-    }
-    else {
-      return results;
+    } else {
+      client.clearScroll({
+        scrollId: [res._scroll_id]
+      })
+        .then(() => {
+          return results;
+        });
     }
   });
 
